001b597ee400a0ac865edd037c3edd38c3351d24
jonathangean <jonathangean@gmail.com>
2014-07-01 04:42:05 -0400

    fontchange

--- app/controllers/poem_controller.rb
+++ app/controllers/poem_controller.rb
@@ -1,36 +1,36 @@
 class PoemController < ApplicationController
   before_filter :authorize
   def new
-      eightyeight_array = Article.find(params[:article_id])
-      new_88_array = []
+    eightyeight_array = Article.find(params[:article_id])
+    new_88_array = []
 
-      lengthcalc = eightyeight_array.article_text.split.select do |word|
-               if word.include? "a"
-                   word
-                elsif word.include? "o"
-                  word
-             end
-          end
-
-      lengthcalc.map do |x|
-        if x.length >= 3
-          new_88_array.push(x)
-        end
-      end
-
-      shuffle88_arr = new_88_array.shuffle
-     string = shuffle88_arr[rand(55..60)..rand(61..70)].join(" ").gsub(/[.()""]/, ' ')
-     @poem1 = string.slice(0,1).capitalize + string.slice(1..-1)
-      string2 = shuffle88_arr[rand(20..30)..rand(30..33)].join(" ").gsub(/[.()""]/, ' ')
-     @poem2 = string2.slice(0,1).capitalize + string2.slice(1..-1)
-     string3 = shuffle88_arr[rand(29..33)..rand(34..38)].join(" ").gsub(/[.()""]/, ' ')
-     @poem3 = string3.slice(0,1).capitalize + string3.slice(1..-1)
-     @list_item1 = shuffle88_arr[rand(20..22)..rand(24..28)]
-     @list_item2 = shuffle88_arr[rand(79..84)..rand(85..90)]
-     @list_item3 = shuffle88_arr[rand(30..34)..rand(35..40)]
-     @list_item4 = shuffle88_arr[rand(50..54)..rand(55..60)]
-     @list_item5 = shuffle88_arr[rand(80..82)..rand(84..86)]
-    @new_arr = shuffle88_arr[rand(1..3)..rand(4..9)]
-
+    lengthcalc = eightyeight_array.article_text.split.select do |word|
+     if word.include? "a"
+       word
+     elsif word.include? "o"
+      word
+    end
   end
+
+  lengthcalc.map do |x|
+    if x.length >= 3
+      new_88_array.push(x)
+    end
+  end
+
+  shuffle88_arr = new_88_array.shuffle
+  string = shuffle88_arr[rand(58..60)..rand(61..72)].join(" ").gsub(/[.()""]/, ' ')
+  @poem1 = string.slice(0,1).capitalize + string.slice(1..-1)
+  string2 = shuffle88_arr[rand(25..28)..rand(29..31)].join(" ").gsub(/[.()""]/, ' ')
+  @poem2 = string2.slice(0,1).capitalize + string2.slice(1..-1)
+  string3 = shuffle88_arr[rand(50..53)..rand(54..56)].join(" ").gsub(/[.()""]/, ' ')
+  @poem3 = string3.slice(0,1).capitalize + string3.slice(1..-1)
+  @list_item1 = shuffle88_arr[rand(20..22)..rand(23..26)]
+  @list_item2 = shuffle88_arr[rand(79..82)..rand(83..85)]
+  @list_item3 = shuffle88_arr[rand(30..32)..rand(33..36)]
+  @list_item4 = shuffle88_arr[rand(50..52)..rand(53..56)]
+  @list_item5 = shuffle88_arr[rand(80..82)..rand(83..85)]
+  @new_arr = shuffle88_arr[rand(1..3)..rand(4..9)]
+
+end
 end
